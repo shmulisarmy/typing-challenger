efc25e09fd6bd3393268cf54de9d97236bce04cb
shmuel keller <shmuli@shmuels-Air.attlocal.net>
2024-10-30 23:22:36 -0400

    removed show_level_buttons function

--- scripts/main.js
+++ scripts/main.js
@@ -12,18 +12,6 @@ let level_upto = 1;
 
 
 
-function show_level_buttons(){
-    return
-
-
-    const next_level_button = document.querySelector(".next");
-    next_level_button.style.display = "block";
-    next_level_button.textContent = "Next level";
-    next_level_button.onclick = function(){
-        next_level_button.style.display = "none";
-        current_level.next();
-    }
-}
 
 
 const global_keyboard_event = (e) => {
@@ -34,7 +22,6 @@ const global_keyboard_event = (e) => {
 
 function on_level_change(){
     document.querySelector(".wpm").textContent = "press any key to start";
-    show_level_buttons();
     window.addEventListener("keydown", global_keyboard_event);
 }
 
